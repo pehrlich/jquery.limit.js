da551fe17c35f34f82591ecaaad712c69e1a60c0
Peter Ehrlich <peter.i.ehrlich@gmail.com>
2012-06-19 08:12:13 -0700

    Unwrap eval'd code in to its own closure.  Change != to !==.

--- jquery.limit.js
+++ jquery.limit.js
@@ -1,31 +1,30 @@
-(function($){ 
-     $.fn.extend({  
-         limit: function(limit,element) {
-			
-			var interval, f;
-			var self = $(this);
-					
-			$(this).focus(function(){
-				interval = window.setInterval(substring,100);
-			});
-			
-			$(this).blur(function(){
-				clearInterval(interval);
-				substring();
-			});
-			
-			substringFunction = "function substring(){ var val = $(self).val();var length = val.length;if(length > limit){$(self).val($(self).val().substring(0,limit));}";
-			if(typeof element != 'undefined')
-				substringFunction += "if($(element).html() != limit-length){$(element).html((limit-length<=0)?'0':limit-length);}"
-				
-			substringFunction += "}";
-			
-			eval(substringFunction);
-			
-			
-			
-			substring();
-			
-        } 
-    }); 
+(function ($) {
+    $.fn.extend({
+        limit:function (limit, element) {
+            var interval;
+            var self = $(this);
+
+            var check_limit = function () {
+                var val = self.val();
+                var length = val.length;
+                if (length > limit) {
+                    self.val(val.substring(0, limit));
+                }
+                if ($(element).html() !== limit - length) {
+                    $(element).html((limit - length <= 0) ? '0' : limit - length);
+                }
+            }
+
+            self.focus(function () {
+                interval = window.setInterval(check_limit, 100);
+            });
+
+            self.blur(function () {
+                clearInterval(interval);
+                check_limit();
+            });
+
+            check_limit();
+        }
+    });
 })(jQuery);
